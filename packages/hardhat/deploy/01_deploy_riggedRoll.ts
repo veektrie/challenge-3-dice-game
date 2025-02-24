ca98d4f61962d844b868d0d50891e0e8d9766d6f
adugbo victory <adugbovictory@gmail.com>
2025-02-24 15:34:42 +0100

    Deploy RiggedRoll contract and enable ownership transfer functionality

--- packages/hardhat/deploy/01_deploy_riggedRoll.ts
+++ packages/hardhat/deploy/01_deploy_riggedRoll.ts
@@ -11,21 +11,21 @@ const deployRiggedRoll: DeployFunction = async function (hre: HardhatRuntimeEnvi
   const diceGameAddress = await diceGame.getAddress();
 
   // Uncomment to deploy RiggedRoll contract
-  // await deploy("RiggedRoll", {
-  //   from: deployer,
-  //   log: true,
-  //   args: [diceGameAddress],
-  //   autoMine: true,
-  // });
+  await deploy("RiggedRoll", {
+    from: deployer,
+    log: true,
+    args: [diceGameAddress],
+    autoMine: true,
+  });
 
-  // const riggedRoll: RiggedRoll = await ethers.getContract("RiggedRoll", deployer);
+  const riggedRoll: RiggedRoll = await ethers.getContract("RiggedRoll", deployer);
 
-  // Please replace the text "Your Address" with your own address.
-  // try {
-  //   await riggedRoll.transferOwnership("Your Address");
-  // } catch (err) {
-  //   console.log(err);
-  // }
+  Please replace the text "Your Address" with your own address.
+  try {
+    await riggedRoll.transferOwnership("Your Address");
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export default deployRiggedRoll;
